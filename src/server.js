52910fae66172c49754c057221f79283322f0a37
IMayCode <imaycode@gmail.com>
2021-09-19 00:19:47 -0500

    Update webpack production config publicPath to /static/

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
-  res.send('Response');
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, () => {
